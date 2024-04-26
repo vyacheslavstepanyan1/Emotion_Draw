c5a9d5e65e832e3bda4f6d90a35ce209669c5cc1
anahit1baghdasarian <andemetra1@gmail.com>
2024-04-26 04:57:16 +0400

    File Name Change

--- Emotion_Draw/bert_part/__init__.py
+++ Emotion_Draw/bert_part/__init__.py
@@ -1,4 +1,4 @@
 from .inference import inference
 from .inference.inference import Inference
-from .model import Multiclass_BERT_RoBERTa
-from .model.Multiclass_BERT_RoBERTa import MulticlassClassificationTrainer
+from .model import Multiclass_BERT
+from .model.Multiclass_BERT import MulticlassClassificationTrainer
