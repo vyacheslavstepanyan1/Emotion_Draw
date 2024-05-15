facbaf846bcbb3c115c92ba2e753043584fcd369
anahit1baghdasarian <andemetra1@gmail.com>
2024-05-15 20:39:07 +0400

    Setup File Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='V.S., A.B.',
+    description='Emotion_Draw',
+    name='Emotion_Draw',
+    version='0.1.0',
+    packages=find_packages(include=['Emotion_Draw','Emotion_Draw.*']),
+    
+)
